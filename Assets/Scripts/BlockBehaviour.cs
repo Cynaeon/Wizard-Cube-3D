c6c796a82d388ac4594609509a35a6e24f2685aa
TAMK\c5jtasal <jonitasala@gmail.com>
2016-10-05 11:51:19 +0300

    Initial committerino

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockBehaviour : MonoBehaviour {
+
+	private Renderer rend;
+	private Color defaultColor;
+	private Color hightlightColor;
+
+	bool blockRaised = false;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		defaultColor = rend.material.color;
+		hightlightColor = new Color (defaultColor.r + 50, defaultColor.b, defaultColor.b, defaultColor.a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver() {
+
+		rend.material.color = hightlightColor;
+
+		if (Input.GetMouseButtonDown (0)) {
+			if (!blockRaised) {
+				Vector3 v = transform.position;
+				v.y = 1f;
+				transform.position = v;
+				blockRaised = true;
+			} else {
+				Vector3 v = transform.position;
+				v.y = 0.5f;
+				transform.position = v;
+				blockRaised = false;
+			}
+		}
+	}
+
+	void OnMouseExit() {
+		rend.material.color = defaultColor; 
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+		transform.LookAt(target);
+	}
+	
+	public Transform target;
+
+	void Update() {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		transform.position += movement * speed;
+
+
+	}
+}
