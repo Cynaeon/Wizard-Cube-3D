2948ad7929ba4459f9ecd0446938b415740ee99e
Tamk Kalle <c5kheino@cs.tamk.fi>
2016-11-02 17:00:33 +0200

    Some beginning for another hole method testing.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,27 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyAI : MonoBehaviour {
+namespace WizardCube
+{
+    public class EnemyAI : MonoBehaviour {
 
-    [SerializeField]
-    private AILerp _aiLerp;
+        [SerializeField]
+        private AILerp _aiLerp;
 
-	// Use this for initialization
-	void Start () {
-	    if (_aiLerp == null)
+	    // Use this for initialization
+	    void Start ()
         {
-            Debug.LogError("EnemyAI reference for AILerp is null.");
-            Debug.Break();
-        }
-	}
+	        if (_aiLerp == null)
+            {
+                Debug.LogError("EnemyAI reference for AILerp is null.");
+                Debug.Break();
+            }
+	    }
 	
-	// Update is called once per frame
-	void Update () {
-        if (transform.position.y <= 0.54f)
+	    // Update is called once per frame
+	    void Update ()
         {
-            _aiLerp.canMove = false;
-            transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
-            Debug.Log("One AI fell into a hole, movement stopped.");
+            if (transform.position.y <= 0.54f)
+            {
+                _aiLerp.canMove = false;
+                transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+                Debug.Log("One AI fell into a hole, movement stopped.");
+            }
+	    }
+
+        // Use this when you want to either make this gameobject move or stop moving
+        public void MovementControl(bool haltMovement)
+        {
+            if (haltMovement)
+            {
+                _aiLerp.canMove = false;
+            }
+            else if (!haltMovement)
+            {
+                _aiLerp.canMove = true;
+            }
         }
-	}
+    }
 }
---
+++ Assets/Scripts/HoleBlockBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WizardCube
+{
+    public class HoleBlockBehaviour : MonoBehaviour
+    {
+        //private BoxCollider _trapCollider;
+
+        private void Awake()
+        {
+            //_trapCollider = GetComponentInChildren<BoxCollider>();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                collision.gameObject.GetComponent<EnemyAI>().MovementControl(true);
+                Destroy(transform.parent.gameObject);
+            }
+        }
+    }
+}
