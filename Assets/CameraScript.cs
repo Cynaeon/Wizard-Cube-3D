cae30655ceef9febab5174801025febec41733ad
TAMK\c5jtasal <jonitasala@gmail.com>
2016-11-15 17:09:41 +0200

    Working on camera script

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float speed = .1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		transform.position += movement * speed;
+	}
+}
