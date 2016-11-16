62dbdc03901307e5aa43e5503ae744083d8bc790
TAMK\c5jtasal <jonitasala@gmail.com>
2016-11-16 13:51:43 +0200

    Some initial camera controls

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	public float speed = 0.25f;
+    public float zoomSpeed = 2f;
+
+    private float cameraMinSize = 4;
+    private float cameraMaxSize = 6;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		transform.position += movement * speed;
+
+        //transform.position = Mathf.Clamp(transform.position.z, -10, 0);
+
+        float fov = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        
+        Camera.main.orthographicSize -= fov;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, cameraMinSize, cameraMaxSize);
+    }
+}
