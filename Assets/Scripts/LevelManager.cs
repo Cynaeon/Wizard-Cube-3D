e1b93622bb39510f787a6dbcb0a59cbcd5e98ec0
KalleTamk <kalle.heinonen@cs.tamk.fi>
2016-11-23 15:23:57 +0200

    Added Level 10 to the build, it is now also accessible from the Stage Select.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,7 +11,7 @@ namespace WizardCube
             Scene currentScene = SceneManager.GetActiveScene();
             int currentBuildIndex = currentScene.buildIndex;
 
-            if (currentScene.buildIndex >= 11)
+            if (currentScene.buildIndex >= 12)
             {
                 MoveToLevelSelect();
             }
