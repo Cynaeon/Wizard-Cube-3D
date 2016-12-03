45a25d8573e37c3aaf61d38304087cea8bc6f55f
Joni Tasala <jonitasala@gmail.com>
2016-12-03 14:40:25 +0200

    Added fonts Added game UI (icons are placeholders (hopefully))

---
+++ Assets/Scripts/UILevelInfo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace WizardCube {
+
+	public class UILevelInfo : MonoBehaviour {
+
+		public Text levelNumber;
+		public Text blocks;
+		public Text turrets;
+		private BlockLimiter _blockLimiter;
+
+		// Use this for initialization
+		void Start () {
+			_blockLimiter = GameObject.Find("BlockController").GetComponent<BlockLimiter>();	
+
+			string levelName = Application.loadedLevelName;
+			levelName = char.ToUpper(levelName[0]) + levelName.Substring(1);
+			levelName = levelName.Insert (5, " ");
+			levelNumber.text = levelName;
+		}
+
+		// Update is called once per frame
+		void Update () {
+			int blocksLeft = _blockLimiter.maxRaisedAmount - _blockLimiter._raised;
+			blocks.text = "x " + blocksLeft;
+			int turretsLeft = _blockLimiter.turretsMax - _blockLimiter._turretsPlaced;
+			turrets.text = "x " + turretsLeft;
+		}
+	}
+}
